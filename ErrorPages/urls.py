0d2d92e02833e482854899f8ab75de59528a40e4
Juan Diaz <20233tn067@utez.edu.mx>
2026-01-20 14:08:31 -0600

    Agregado archivo de Juan para la actividad

--- ErrorPages/urls.py
+++ ErrorPages/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from core import views as core
 
 urlpatterns = [
-    path('',core.index,name='index'),
+    path('', core.index, name='index'),
     path('onePage/', core.onePage, name='onePage'),
-    path('derick/', core.derick, name='derick')
+    path('juan/', core.juan, name='juan'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    #Código backend
+    #Codigo backend
     return render(request, 'core/index.html')
 
 def onePage(request):
     return render(request, 'core/onePage.html')
 
-def derick(request):
-    print("")
-    return render(request, 'core/derick.html')
+def juan(request):
+    return render(request, 'core/juan.html')
