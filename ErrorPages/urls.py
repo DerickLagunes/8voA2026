a8b81136a1b2063d9b7e9c05c77beb846ac35218
Derick Axel Lagunes Ramirez <dericklagunes@utez.edu.mx>
2026-01-19 20:15:06 -0600

    primer commit

---
+++ ErrorPages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from core import views as core
+
+urlpatterns = [
+    path('',core.index,name='index'),
+    path('onePage/', core.onePage, name='onePage'),
+    path('derick/', core.derick, name='derick'),
+]
---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    #Código backend
+    return render(request, 'core/index.html')
+
+def onePage(request):
+    return render(request, 'core/onePage.html')
+
+def derick(request):
+    return render(request, 'core/derick.html')
